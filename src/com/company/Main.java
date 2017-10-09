4d7d19659dbf0795ce56e0418c3b5cd937b16926
katya <yaschinaea@mail.ru>
2017-10-09 22:35:21 +0400

    Use another file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,60 +9,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final String FILENAME = "C:\\Users\\katya\\KatyaBot\\src\\com\\company\\answers.txt";
+    private static String FILENAME = "C:\\Users\\katya\\KatyaBot\\src\\com\\company\\answers.txt";
 
-    public static void main(String[] args) {
-
+    private static ArrayList<String> GetFile() {
         BufferedReader br = null;
         FileReader fr = null;
-        String lastLine;
 
         try {
             fr = new FileReader(FILENAME);
             br = new BufferedReader(fr);
-            Scanner scanner = new Scanner(System.in);
             ArrayList<String> array = new ArrayList<>();
-            Random rand = new Random();
-
-            System.out.println(br.readLine());
-
             String sCurrentLine;
             while ((sCurrentLine = br.readLine()) != null) {
                 array.add(sCurrentLine);
             }
-
-            boolean talk = true;
-            lastLine = array.get(array.size()-1);
-
-            while (true) {
-
-                String input = scanner.nextLine();
-
-                if ("GoodBuy".equals(input)) {
-                    System.out.println(lastLine);
-                    break;
-                }
-
-                if ("Stop talking".equals(input)) {
-                    talk = false;
-                }
-
-                if ("Start talking".equals(input)) {
-                    talk = true;
-                    continue;
-                }
-
-                if(talk) {
-                    int randomIndex = rand.nextInt(array.size()-1);
-                    System.out.println(array.get(randomIndex));
-                }
-
-            }
-
-            scanner.close();
+            return array;
         } catch (IOException e) {
-
             e.printStackTrace();
+            return new ArrayList<>();
 
         } finally {
 
@@ -81,7 +45,53 @@ public class Main {
             }
 
         }
+    }
 
+    public static void main(String[] args) {
+
+
+        String lastLine;
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        ArrayList<String> array = GetFile();
+
+        System.out.println(array.get(0));
+
+        boolean talk = true;
+        lastLine = array.get(array.size() - 1);
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.contains("Use another file:")) {
+                FILENAME = input.substring(17);
+                array = GetFile();
+            }
+
+            if ("GoodBuy".equals(input)) {
+                System.out.println(lastLine);
+                break;
+            }
+
+            if ("Stop talking".equals(input)) {
+                talk = false;
+            }
+
+            if ("Start talking".equals(input)) {
+                talk = true;
+                continue;
+            }
+
+            if (talk) {
+                int randomIndex = rand.nextInt(array.size() - 2) + 1;
+                System.out.println(array.get(randomIndex));
+            }
+
+        }
+
+        scanner.close();
     }
 
 }
+
